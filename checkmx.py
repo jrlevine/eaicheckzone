c62c670327df25edde507ff6f2a07022e88eca8a
John Levine <john.levine@standcore.com>
2020-05-04 21:36:56 -0400

    add copyright

--- checkmx.py
+++ checkmx.py
@@ -3,6 +3,16 @@
 # threaded version
 # put summary in database
 
+# Copyright 2019-2020 Standcore LLC
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import dns.resolver
 import argparse
 import os
@@ -179,7 +189,7 @@ if __name__ == '__main__':
         # put summary in database
         tld =  zoneroot[1:-1]
         print("Record {} ndom {} nmx {}".format(tld,ndom,nmx), file=sys.stderr)
-        db = pymysql.connect(unix_socket='/tmp/mysql.sock',user='eaimail',password='x',db='eaimail')
+        db = pymysql.connect(user='eaimail',password='x',db='eaimail')
         with db.cursor() as cur:
             nname = None                # keep nname if it's set
             if cur.execute("""SELECT nname FROM eaitld WHERE tld=%s""", (tld,)):
--- testmx.py
+++ testmx.py
@@ -2,6 +2,16 @@
 # test MXes
 # thread version
 
+# Copyright 2019-2020 Standcore LLC
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import dns.resolver
 import argparse
 import sys
@@ -161,7 +171,7 @@ def putaway(replace=False):
         """ ipv4 to unsigned int """
         return struct.unpack(">I", socket.inet_aton(s))[0]
 
-    db = pymysql.connect(host='g4.iecc.com',user='eaimail',password='x',db='eaimail')
+    db = pymysql.connect(user='eaimail',password='x',db='eaimail')
     ex = [ [oldtld, aton(a)]+i for (a,i) in eai.items() if len(i) == 5]
     print("to database",ex)
     with db.cursor() as cur:
